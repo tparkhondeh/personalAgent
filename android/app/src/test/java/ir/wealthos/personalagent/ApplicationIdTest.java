467cbe72fec7a54ff2f217c1b29c4acbf81f4371
tparkhondeh <314977283+tparkhondeh@users.noreply.github.com>
2026-09-05 10:05:34 -0700

    test: support isolated Android preview identities

--- android/app/src/test/java/ir/wealthos/personalagent/ApplicationIdTest.java
+++ android/app/src/test/java/ir/wealthos/personalagent/ApplicationIdTest.java
@@ -1,12 +1,12 @@
 package ir.wealthos.personalagent;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class ApplicationIdTest {
     @Test
-    public void applicationIdIsStable() {
-        assertEquals("ir.wealthos.personalagent", BuildConfig.APPLICATION_ID);
+    public void applicationIdUsesHamrahNamespace() {
+        assertTrue(BuildConfig.APPLICATION_ID.startsWith("ir.wealthos.personalagent"));
     }
 }
